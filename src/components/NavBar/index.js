fcc76e0c2273b5168dfd5fc7b4d6f27be8ab1648
Marcelo Amarante Cintra <est-marcelo.cintra@tjsp.jus.br>
2024-09-18 12:48:05 -0300

    NavBar - incomplete

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,17 @@
 const NavBar = () => {
 
     return (
-        <nav className="navbar bg-body-tertiary">
+        <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
             <div className="container">
                 <a className="navbar-brand fs-4" href="#">
                     <img src="/images/logo.png" width={50} className="me-1" />
                     FilmScout
                 </a>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">Now Playing</a>
-                        <a className="nav-link" href="#">Top Rated</a>
-                    </div>
-                </div>
+
+                <a className="nav-link" href="#">Home</a>
+                <a className="nav-link" href="#">Now Playing</a>
+                <a className="nav-link" href="#">Top Rated</a>
+
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Search for a movie..." aria-label="Search" />
                     <button className="btn btn-outline-info" type="submit">Search</button>
