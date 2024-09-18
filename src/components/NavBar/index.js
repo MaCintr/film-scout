2f6dd58b74290e62e0469bf75a739827c2062339
Marcelo Amarante Cintra <est-marcelo.cintra@tjsp.jus.br>
2024-09-18 12:34:31 -0300

    First Commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,26 @@
+const NavBar = () => {
+
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container">
+                <a className="navbar-brand fs-4" href="#">
+                    <img src="/images/logo.png" width={50} className="me-1" />
+                    FilmScout
+                </a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link" href="#">Now Playing</a>
+                        <a className="nav-link" href="#">Top Rated</a>
+                    </div>
+                </div>
+                <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search for a movie..." aria-label="Search" />
+                    <button className="btn btn-outline-info" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
