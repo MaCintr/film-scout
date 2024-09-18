ffba8ae10df4b0f8fefa21157092f134160e4e4b
MaCintr <marcelocintra2307@gmail.com>
2024-09-18 18:47:43 -0300

    React BS - NavBar design complete

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,26 +1,49 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 const NavBar = () => {
 
     return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container">
-                <a className="navbar-brand fs-4" href="#">
-                    <img src="/images/logo.png" width={50} className="me-1" />
-                    FilmScout
-                </a>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">Now Playing</a>
-                        <a className="nav-link" href="#">Top Rated</a>
-                    </div>
-                </div>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search for a movie..." aria-label="Search" />
-                    <button className="btn btn-outline-info" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-    )
+        <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+          <Container>
+            <Navbar.Brand href="#">
+                <img src="/images/logo.png"
+                alt='FilmScout logo'
+                height={40}
+                width={40}
+                className='me-2'
+                />
+                FilmScout
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+                <Nav.Link href="#">Home</Nav.Link>
+                <Nav.Link href="#">Top Rated</Nav.Link>
+                <Nav.Link href="#">
+                  About
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search for a movie..."
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-dark">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
 }
 
 export default NavBar
